03b331d60cfdf62bcadcc5edd1d736c575a1a8f0
BNTang <303158131@qq.com>
2024-12-29 14:38:43 +0800

    feat(demo): 添加首页和关于页面
    - 新增 about 和 home 页面的样式文件
    - 创建 about 和 home 组件
    - 添加路由配置文件 .umirc.js

---
+++ demo/.umirc.js
@@ -0,0 +1,7 @@
+export default {
+    routes: [
+        {exact: true, path: '/', component: 'home'},
+        {exact: true, path: '/home', component: 'about'},
+        {exact: true, path: '/about', component: 'index'},
+    ],
+}
